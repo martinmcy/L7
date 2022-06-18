ff43f8f0287cb63960fb29ce70a67a75fb6e15f5
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-06-18 12:08:37 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+input.onGesture(Gesture.LogoUp, function () {
+	
+})
+input.onGesture(Gesture.LogoDown, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+input.onGesture(Gesture.ScreenDown, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+basic.forever(function () {
+	
+})
